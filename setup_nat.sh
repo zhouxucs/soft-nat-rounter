a55f37e7191b3871591f82bab3b5065cc03f9dcb
Xu Zhou <zhouxunudt@gmail.com>
2018-05-18 00:43:34 +0000

    open iptables first.

--- setup_nat.sh
+++ setup_nat.sh
@@ -2,7 +2,7 @@ WAN=wlan0
 LAN=eth0
 #PUBLIC_IP=172.20.10.5
 INTERNAL_SUB_NET=10.0.0.0/24
-
+# echo 1 > /proc/sys/net/ipv4/ip_forward
 #sudo iptables -t nat -A POSTROUTING -s ${INTERNAL_SUB_NET} -j SNAT --to-source ${PUBLIC_IP}
 #sudo iptables -A FORWARD -s ${INTERNAL_SUB_NET} -j ACCEPT
 sudo iptables -t nat -A POSTROUTING -s ${INTERNAL_SUB_NET} -o ${WAN} -j MASQUERADE
